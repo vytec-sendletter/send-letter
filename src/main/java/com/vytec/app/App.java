4cc9dd646d2b11e7e524138782d00b34dab658b3
Ravi <Ravi@vytec.com>
2022-04-19 01:13:41 +0000

    updated

---
+++ src/main/java/com/vytec/app/App.java
@@ -0,0 +1,13 @@
+package com.vytec.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
